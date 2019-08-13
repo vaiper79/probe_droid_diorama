d54ea57341599ba495e6fb4bfcf95ae1d1690465
Ole A <olestordal@gmail.com>
2019-08-13 21:21:19 +0200

    Version 3: Changed to DAC circuit for volume

--- droid.py
+++ droid.py
@@ -1,6 +1,15 @@
 # Python3
 
-import pygame, random, time, os
+# Version 3, 13.08.2019, OleA aka vaiper79
+# Volume configuration by Adafruit:
+# Hardware: MCP3008 DAC Chip 
+# Code: https://learn.adafruit.com/reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/overview
+
+
+
+import pygame, random, time, os, busio, digitalio, board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 from gpiozero import Button, PWMLED
 
 # Pintout:
@@ -11,20 +20,29 @@ from gpiozero import Button, PWMLED
 # #4: 19
 # #5: 26
 # 
-# Buttons:
-# Volume+: 17
-# Volume-: 18
-# Selector: 16
+# Button:
+# Selector: 17
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D22)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan0 = AnalogIn(mcp, MCP.P0)
 
 # Pins defined
-button = Button(16, hold_time=3)    
-volumeUp = Button(17)                      # Rotary encoder pin A connected to GPIO2
-volumeDown = Button(18)                      # Rotary encoder pin B connected to GPIO3
+button = Button(17, hold_time=3)    
 led1 = PWMLED(5)
 led2 = PWMLED(6)
 led3 = PWMLED(13)
 led4 = PWMLED(19)
 led5 = PWMLED(26)
+
 
 pygame.init() # Required to get_ticks() since start
 
@@ -65,26 +83,27 @@ if __name__ == "__main__":
     lastTelemetryMillis = 0
     brightness=0.4
     fadeAmount=0.001
+    last_read = 0       # this keeps track of the last potentiometer value
+    tolerance = 250     # to keep from being jittery we'll only change
+                        # volume when the pot has moved a significant amount
+                        # on a 16-bit ADC
 
     def doIt(): # Example...
         print ("Just Do It!!!")
         #exit()
 
-    def volume_down():                    # Pin A event handler
-        print ("Volume Down")
-        pygame.mixer.music.set_volume(pygame.mixer.music.get_volume()-0.1)
-        #pygame.mixer.Channel(1).set_volume(pygame.mixer.Channel(1).get_volume()-0.1)
-        pygame.mixer.Channel(2).set_volume(pygame.mixer.Channel(2).get_volume()-0.1)
-        pygame.mixer.Channel(3).set_volume(pygame.mixer.Channel(3).get_volume()-0.1)
-        pygame.mixer.Channel(4).set_volume(pygame.mixer.Channel(4).get_volume()-0.1)
-        
-    def volume_up():                  
-        print ("Volume Up")
-        pygame.mixer.music.set_volume(pygame.mixer.music.get_volume()+0.1)
-        #pygame.mixer.Channel(1).set_volume(pygame.mixer.Channel(1).get_volume()+0.1)
-        pygame.mixer.Channel(2).set_volume(pygame.mixer.Channel(2).get_volume()+0.1)
-        pygame.mixer.Channel(3).set_volume(pygame.mixer.Channel(3).get_volume()+0.1)
-        pygame.mixer.Channel(4).set_volume(pygame.mixer.Channel(4).get_volume()+0.1)
+    def remap_range(value, left_min, left_max, right_min, right_max):
+        # this remaps a value from original (left) range to new (right) range
+        # Figure out how 'wide' each range is
+        left_span = left_max - left_min
+        right_span = right_max - right_min
+
+        # Convert the left range into a 0-1 range (int)
+        valueScaled = int(value - left_min) / int(left_span)
+
+        # Convert the 0-1 range into a value in the right range.
+        return int(right_min + (valueScaled * right_span))
+
 
     while True:
 
@@ -101,14 +120,31 @@ if __name__ == "__main__":
         led4.value = brightness
         led5.value = brightness
                 
-        if volume >= 1:
-            volume = 1
-        elif volume <= 0:
-            volume = 0
+        # we'll assume that the pot didn't move
+        trim_pot_changed = False
 
-        button.when_released = doIt # Do stuff as it is being released..next action etc..
-        volumeDown.when_released = volume_down
-        volumeUp.when_released = volume_up
+        # read the analog pin
+        trim_pot = chan0.value
+
+        # how much has it changed since the last read?
+        pot_adjust = abs(trim_pot - last_read)
+
+        if pot_adjust > tolerance:
+            trim_pot_changed = True
+
+        if trim_pot_changed:
+            # convert 16bit adc0 (0-65535) trim pot read into 0-100 volume level
+            set_volume = remap_range(trim_pot, 0, 65535, 0, 100)
+
+            # set OS volume playback volume
+            print('Volume = {volume}%' .format(volume = set_volume))
+            set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' \
+            .format(volume = set_volume)
+            os.system(set_vol_cmd)
+
+            # save the potentiometer reading for the next loop
+            last_read = trim_pot
+
 
         if button.is_held:  # Used to exit when held for hold time, which now is 3 seconds
             exit()
